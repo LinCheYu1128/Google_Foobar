0855b3cfb343ef0d922d41d0ff880f78cb9915d2
LinCheYu1128 <jonathan871128@gmail.com>
2024-01-19 00:52:34 +0800

    lv4 q2 fin

--- Level4/running_with_bunnies/solution.py
+++ Level4/running_with_bunnies/solution.py
@@ -14,80 +14,49 @@ def solution(times, time_limit):
     # for i in range(num_vertices):
     #     print times[i]
 
+    # times_limit is very large
     if time_limit >= 999:
         return list(range(num_bunnies))
-
-    # Initialize the set of bunnies to save
-    bunny_set_to_save = []
-
-    # Helper function to check if a given set of bunnies is better than the current best
-    def is_better_set(candidate_set):
-        return (
-            len(candidate_set) > len(bunny_set_to_save) or
-            (len(candidate_set) == len(bunny_set_to_save) and candidate_set < bunny_set_to_save)
-        )
+    # Negative loop
+    else:
+        for i in range(num_bunnies+1):
+            if (times[i][i] < 0):
+                return list(range(num_bunnies))
 
     # Generate all possible sets of bunnies (excluding the start and bulkhead)
     all_bunny_sets = []
-    for i in range(num_bunnies+1):
-        for combo in itertools.permutations(range(i)):
+    for i in range(num_bunnies,-1,-1):
+        for combo in itertools.permutations(range(num_bunnies),i):
             all_bunny_sets.append(combo)
     # print all_bunny_sets
-
+    
     # # Iterate through all bunny sets and find the best set within the time limit
-    # for bunny_set in all_bunny_sets:
-    #     current_time_limit = time_limit
-
-    #     # Calculate time to reach each bunny from the start and to reach bulkhead from each bunny
-    #     bunny_times = [times[0][bunny + 1] for bunny in bunny_set] 
-    #     bulkhead_times = [times[bunny + 1][-1] for bunny in bunny_set]
-    #     print bunny_times, bulkhead_times
-        # # Sort the bunnies by their arrival time to optimize the order of picking them up
-        # sorted_bunny_set = [bunny for _, bunny in sorted(zip(bunny_times, bunny_set))]
-        # print sorted_bunny_set
-
-        # # Calculate total time required to pick up and reach the bulkhead with the sorted bunny set
-        # total_time = sum(bunny_times) + sum(bulkhead_times)
-
-        # # Check if the total time is within the limit and if the current set is better
-        # if total_time <= current_time_limit and is_better_set(sorted_bunny_set):
-        #     bunny_set_to_save = sorted_bunny_set
-
-    return sorted(bunny_set_to_save)
-    # all_bunnies = [0 for i in range(n-2)]
-    # dist = times[0]
-    # graph = []
-    # for i in range(n):
-    #     for j in range(n):
-    #         graph.append([i, j, times[i][j]])
-    # # print graph
-    # for i in range(n):
-    #     for
-    # def get_all_bunnies():
-    #     for i in all_bunnies:
-    #         if i == 0:
-    #             return 0
-    #     return 1 
-
-    
-    
-    # return times
-
-
+    for bunny_set in all_bunny_sets:
+        current_time_limit = time_limit
+        current_state = -1
+        success = 1
+        for next_state in bunny_set:
+            current_time_limit -= times[current_state+1][next_state+1]
+            if current_time_limit < 0 and current_time_limit - times[next_state+1][num_bunnies+1] < 0:
+                success = 0
+                break
+            current_state = next_state
+        
+        if success == 1 and current_time_limit - times[current_state+1][num_bunnies+1] >= 0:
+            return sorted(bunny_set)
+    return []
 
 import time
-# start = time.time()
-# times = [[0, 1, 1, 1, 1], 
-#          [1, 0, 1, 1, 1], 
-#          [1, 1, 0, 1, 1], 
-#          [1, 1, 1, 0, 1], 
-#          [1, 1, 1, 1, 0]]
-# times_limit = 3
-# ans = solution(times, times_limit)
-# end = time.time()
-# print 'ans =', ans, ', expect =', [0,1] == ans, ', time = %f'%(end - start)
-
-
+start = time.time()
+times = [[0, 1, 1, 1, 1], 
+         [1, 0, 1, 1, 1], 
+         [1, 1, 0, 1, 1], 
+         [1, 1, 1, 0, 1], 
+         [1, 1, 1, 1, 0]]
+times_limit = 3
+ans = solution(times, times_limit)
+end = time.time()
+print 'ans =', ans, ', expect =', [0,1] == ans, ', time = %f'%(end - start)
 
 start = time.time()
 times = [[0, 2, 2, 2, -1], 
